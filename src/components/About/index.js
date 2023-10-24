dc2e79567fce1745a7aa9b6afe4a63c18a99d01f
Tristan Lewis <valar_morghulis@outlook.com>
2023-10-23 21:39:01 -0400

    Initial commit

---
+++ src/components/About/index.js
@@ -0,0 +1,29 @@
+import './index.scss';
+import { useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
+import Loader from 'react-loaders';
+
+const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    return (
+        <>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                            idx={15} />
+                    </h1>
+                    <p>As a web developer with a knack for creativity, I thoroughly enjoy developing appealing, user-friendly web solutions</p>
+                    <p>Although I excell working with client-side technologies, I have a well rounded skillset with experience in design and server-side technologies as well!</p>
+                    <p>My unique background in management provides me with invaluable skills such as time management, organization, and effective communication just to name a few.</p>
+                </div>
+            </div>
+            <Loader type='ball-scale-ripple-multiple'/>
+        </>
+    )
+}
+
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import './index.scss';
+import { useState } from "react";
+import AnimatedLetters from "../AnimatedLetters";
+import Loader from "react-loaders";
+import Image from '../../assets/images/rochester-skyline2.png'
+
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['', 'T', 'r', 'i', 's', 't', 'a', 'n', ' ', 'L', 'e', 'w', 'i', 's'];
+    const jobArray = ['A', ' ', 'w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r'];
+    return (
+        <>
+            <div className='container home-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <span className={letterClass}>I'm</span>
+                        <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={17} />
+                        <br />
+                        <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={21} />
+                    </h1>
+                    <h2>Frontend Developer | Animal Lover | Gamer </h2>
+                    <div className='flex-buttons'>
+                        <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+                        <Link to='/contact' className='flat-button'>SEE MY WORK</Link>
+                    </div>
+                </div>
+            </div>
+            <img src={Image} alt='rochester skyline' />
+            <Loader type='ball-scale-ripple-multiple' />
+        </>
+    )
+}
+
+export default Home
